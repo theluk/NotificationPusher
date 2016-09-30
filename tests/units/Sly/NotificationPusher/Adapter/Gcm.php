be62dcbda749ab6c403394c549cbe90cfabe2774
theluk <theluk@gmail.com>
2016-09-30 18:36:14 +0200

    update tests and fix #125

--- tests/units/Sly/NotificationPusher/Adapter/Gcm.php
+++ tests/units/Sly/NotificationPusher/Adapter/Gcm.php
@@ -74,11 +74,11 @@ class Gcm extends Units\Test
             ->array($definedParameters = $object->getDefinedParameters())
             ->isNotEmpty()
             ->containsValues(array(
-                'collapse_key',
-                'delay_while_idle',
-                'time_to_live',
-                'restricted_package_name',
-                'dry_run'
+                'collapseKey',
+                'delayWhileIdle',
+                'ttl',
+                'restrictedPackageName',
+                'dryRun'
             ))
         ;
     }
